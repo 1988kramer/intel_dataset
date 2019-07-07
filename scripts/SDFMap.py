969661433ca70d19f428a2f2931ee2324b05870a
Andrew Kramer <1988kramer@gmail.com>
2019-07-07 13:51:29 -0700

    added functions to get vertices to be updated and fit line to points in a given map cell. started on function to calculate updates and their priorities

--- scripts/SDFMap.py
+++ scripts/SDFMap.py
@@ -23,8 +23,10 @@ class SDFMap:
 	# - size:           tuple, stores the starting spatial extents of the map 
 	#                   in the x and y directions in meters
 	# - discretization: float, the edge length of a single map cell in meters
+	# - k:              max radius in which to update vertices in meters
 	#
-	def __init__(self, size, discretization=0.5):
+	def __init__(self, size, discretization=0.5, k=1.0):
+		self.k = k
 		self.num_x_cells = int(size[0] / discretization)
 		self.num_y_cells = int(size[1] / discretization)
 		self.map = np.zeros((self.num_x_cells,self.num_y_cells))
@@ -50,12 +52,78 @@ class SDFMap:
 		# generate map updates for each cell group
 		for group in point_groups:
 
-			# get linear fit for current point group
 			A,b = self.LinearFit(group, pose)
 
-			# get vertices to be updated
+			vertices = self.GetUpdateVertices(A, group[0])
 
-		
+			updates = self.GetDistAndPriority(vertices, A, b, pose)
+
+
+	# calculates the shortest distance to an obstacle and the update priority
+	# for the given list of vertices
+	# params:
+	# - vertices: list of vertices for which to calculate updates
+	# - A:        slope of the line fitted to the scan endpoints in the cell
+	# - b:        y intercept of the line fitted to the scan endpoints in the cell
+	# - pose:     pose of the robot
+	# returns:
+	# - updates:    list of updated distances for each vertex
+	# - priorities: list of priorities for each update
+	def GetDistAndPriority(vertices, A, b, pose):
+
+
+
+	# get list of vertex indices to update per requirements 
+	# in section III.A of Fossel et al
+	# params:
+	# - A:      slope of line fitted to points in cell
+	# - point:  a point inside the cell
+	# returns:
+	# - indices: list of tuples; vertex indices to be updated
+	def GetUpdateVertices(self, A, point):
+
+		# get vertices bounding the current cell
+		x_min,x_max,y_min,y_max = self.GetBoundingVertices(point)
+		x_c = (x_max + x_min) / 2.0
+		y_c = (y_max + y_min) / 2.0
+
+		# get lines bounding the cells to update
+		A_prime = -1.0 / A # slope of both bounding lines
+		b_lower = 0.0 # y intercept of lower line in cells (not meters)
+		b_upper = 0.0 # y intercept of upper line in cells (not meters)
+		if A_prime < 0:
+			b_lower = y_min - (A_prime * x_max)
+			b_upper = y_max - (A_prime * x_min)
+		else:
+			b_lower = y_min - (A_prime * x_min)
+			b_upper = y_max - (A_prime * x_max)
+
+		# search nearby vertices and create list of vertices to update
+		# assumes updated cells never go off the edge of the map
+		# need to make function to expand map in this case
+		indices = []
+		k_cells = int(self.k / self.discretization)
+		x_range_min = int(x_c - k_cells)
+		x_range_max = int(x_c + k_cells)
+		y_range_min = int(y_c - k_cells)
+		y_range_max = int(y_c + k_cells)
+		for x in range(x_range_min, x_range_max):
+			for y in range(y_range_min, y_range_max):
+
+				# rule out vertices based on distance to cell center
+				dist = math.sqrt((x - x_c)**2 + (y - y_c)**2)
+				if dist >= k_cells:
+					continue
+
+				# rule out cells not between the two bounding lines
+				Ax = A_prime * x
+				if Ax + b_upper < y or Ax + b_lower > y:
+					continue
+
+				indices.append((x,y))
+
+		return indices
+
 
 	# fits a line to the given list of points using orthogonal regression
 	# if the list contains only one point, the line is perpendicular to 
