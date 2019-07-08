7a88e3294549ef2d779e364b9dc4ecfdcab3a312
Andrew Kramer <1988kramer@gmail.com>
2019-07-08 16:53:08 -0700

    development on scan alignment function

--- scripts/SDFScanMatcher.py
+++ scripts/SDFScanMatcher.py
@@ -30,11 +30,37 @@ class SDFScanMatcher:
 	# params:
 	# - scan: the new scan's endpoints in the robot's frame of reference
 	#		  2D numpy array with one point per row 
-	#.        (expressed in homogeneous coordinates)
+	#         (expressed in homogeneous coordinates)
 	# - pose_delta_guess: an initial guess of the change in the robot's 
-	#.                    pose since the last scan
-	def AddScan(self, scan, pose_delta_guess=np.identity(3)):
+	#                     pose since the last scan
+	# - max_iter: int, maximum number of Gauss-Newton iterations to use
+	# - min_d_err: float, minimum change in error since last iteration to continue iterating
+	def AddScan(self, scan, pose_delta_guess=np.identity(3), max_iter=10, min_d_err=1.0e-4):
+		
+		err = 1.0e4
+		d_err = 1.0e4
+		num_iter = 0
+
+		next_pose = np.dot(self.pose,pose_delta_guess)
+
+		while num_iter < max_iter and d_err > min_d_err:
+
+			# transform points to global frame
+			gf_scan = np.dot(scan,next_pose.T)
+
+			# get map values and gradients for every scan endpoint
+			vals = np.zeros(scan.shape[0])
+			grads = np.zeros((scan.shape[0],2))
+			for scan_idx in range(scan.shape[0]):
+				val,grad = self.map.GetMapValueAndGradient(gf_scan[scan_idx,:2])
+				vals[scan_idx] = val
+				grads[scan_idx,:] = grad
+
+			# assemble measurement and jacobian matrices
+
+			# calculate the pose update
+
+			# evaluate the error
 
 
-	#
-
+			num_iter += 1
