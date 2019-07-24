99b54aa31f9dca97bbfaa2403fc767b94a33eb28
Andrew Kramer <1988kramer@gmail.com>
2019-07-24 11:11:31 -0700

    further testing to map alignment algorithm, gradients seem to be working as expected but can result in outliers in sparse areas of the map. may need to implement some sort of smoothing in these areas

--- scripts/TestSDF.py
+++ scripts/TestSDF.py
@@ -17,14 +17,14 @@ args = parser.parse_args()
 
 loader = DataLoader(args.laser_file, args.odometry_file)
 measurements = loader.measurements
-disc = 0.5
+disc = 0.25
 matcher = SDFScanMatcher(discretization=disc)
 matcher.AddScan(measurements[0].points)
 matcher.AddScan(measurements[1].points)
 #matcher.AddScan(measurements[2].points)
-res,J,grads = matcher.GetResidualAndJacobian(measurements[0].points,np.identity(3))
+res,J,grads = matcher.GetResidualAndJacobian(measurements[34].points,np.identity(3))
 #sdf = SDFMap([10,10])
-print("residual on initial map: {:f}".format(np.linalg.norm(res**2)))
+print("residual on next scan: {:f}".format(np.linalg.norm(res**2)))
 
 fig = plt.figure()
 
@@ -32,7 +32,7 @@ plt.imshow(matcher.map.map, interpolation='none',vmin=-1.5,vmax=1.5)
 plt.grid(True, 'major')
 plt.colorbar()
 
-map_space_points = measurements[0].points / disc
+map_space_points = measurements[34].points / disc
 map_space_points[:,0] += matcher.map.offsets[0]
 map_space_points[:,1] += matcher.map.offsets[1]
 plt.scatter(map_space_points[:,1],map_space_points[:,0], c='r', s=0.5)
@@ -41,9 +41,10 @@ plt.scatter(map_space_points[:,1],map_space_points[:,0], c='r', s=0.5)
 
 for i in range(0,map_space_points.shape[0]):
 	plt.arrow(map_space_points[i,1],map_space_points[i,0],grads[i,1]*res[i,0],grads[i,0]*res[i,0],width=1.0e-4,color='r')
-
+'''
 fig2 = plt.figure()
 plt.imshow(matcher.map.priorities, interpolation='none',vmin=0,vmax=10)
+'''
 '''
 def animate(i):
 	global pose
